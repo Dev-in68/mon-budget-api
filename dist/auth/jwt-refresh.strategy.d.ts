8f1e35cc10ce1896c018b00baf97ffff56a973ee
anthony <anthony68990@orange.fr>
2025-10-17 23:28:33 +0200

    Corrections backend - scripts et dépendances complets

--- dist/auth/jwt-refresh.strategy.d.ts
+++ dist/auth/jwt-refresh.strategy.d.ts
@@ -1,5 +1,5 @@
 import { Strategy } from 'passport-jwt';
-declare const JwtRefreshStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
+declare const JwtRefreshStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptions] | [opt: import("passport-jwt").StrategyOptions]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class JwtRefreshStrategy extends JwtRefreshStrategy_base {
